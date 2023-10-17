93c5bb8b07de5aa2989ef4f06c4d638fb5b0064e
ToCraft <tobias.cuiper@gmx.de>
2023-10-17 17:21:56 +0200

    added commandRegistration event

---
+++ fabric/src/main/java/tocraft/craftedcore/fabric/events/common/FabricEventHandler.java
@@ -0,0 +1,11 @@
+package tocraft.craftedcore.fabric.events.common;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import tocraft.craftedcore.events.common.CommandEvents;
+
+public class FabricEventHandler {
+
+	public FabricEventHandler() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registry, selection) -> CommandEvents.REGISTRATION.invoker().register(dispatcher, registry, selection));
+	}
+}
--- forge/src/main/java/tocraft/craftedcore/forge/events/common/ForgeEventHandler.java
+++ forge/src/main/java/tocraft/craftedcore/forge/events/common/ForgeEventHandler.java
@@ -1,9 +1,11 @@
 package tocraft.craftedcore.forge.events.common;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import tocraft.craftedcore.events.common.CommandEvents;
 import tocraft.craftedcore.events.common.PlayerEvents;
 
 public class ForgeEventHandler {
@@ -11,5 +13,10 @@ public class ForgeEventHandler {
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public static void event(PlayerLoggedInEvent event) {
 		PlayerEvents.PLAYER_JOIN.invoker().join((ServerPlayer) event.getEntity());
-	}	
+	}
+	
+	@SubscribeEvent(priority = EventPriority.HIGH)
+    public static void event(RegisterCommandsEvent event) {
+        CommandEvents.REGISTRATION.invoker().register(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection());
+    }
 }
