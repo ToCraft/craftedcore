16145e325d2d95d68d2d4887cf888fa795ec84bb
ToCraft <tobias.cuiper@gmx.de>
2023-10-18 16:45:22 +0200

    imrpoved publishing

--- autoupload.bat
+++ autoupload.bat
@@ -2,7 +2,7 @@
 set /p version=Please enter the version (needed for github, also check gradle.properties): 
 
 :: add mcversion here
-set mcversions="1.20.1"
+set mcversions="1.20.1";"1.19.4"
 :: add loader here
 set modloader="fabric";"forge"
 
@@ -11,11 +11,11 @@ for %%m in (%mcversions%) DO (
     git checkout "%%m"
     gh release create "%version%-%%m" --generate-notes
 
+    .\gradlew publish
     for %%l in (%modloader%) DO (
         call .\gradlew %%l:build
         .\gradlew %%l:modrinth
         .\gradlew %%l:curseforge
-        .\gradlew %%l:publish
         move "%%l\build\libs\craftedcore-1.8-%%l.jar" "%%l\build\craftedcore-%%m-%%l-%version%.jar"
         rmdir /s /q "%%l\build\libs"
         gh release upload "%version%-%%m" "%%l\build\craftedcore-%%m-%%l-%version%.jar"
