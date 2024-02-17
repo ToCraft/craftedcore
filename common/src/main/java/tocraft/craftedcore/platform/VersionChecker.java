0b5aa781660a632b7be01577eb0566d466dbc0af
ToCraft <piminecraft492@gmail.com>
2024-02-17 12:23:37 +0100

    add cache option to version checker

--- common/src/main/java/tocraft/craftedcore/platform/VersionChecker.java
+++ common/src/main/java/tocraft/craftedcore/platform/VersionChecker.java
@@ -11,44 +11,60 @@ import java.io.InputStreamReader;
 import java.lang.module.ModuleDescriptor;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 public class VersionChecker {
+    private static final Map<String, String> CACHED_VERSION = new HashMap<>();
+
     public static void registerMavenChecker(String modid, URL mavenURL, Component modName) {
         registerChecker(modid, mavenURL, "<version>" + Platform.getMinecraftVersion() + "-", "</version>", modName);
     }
 
     public static void registerChecker(String modid, URL urlToCheck, String linePrefix, String lineSuffix, Component modName) {
+        // cache versions in extra thread to long loading times
+        new Thread(() -> cacheNewestVersion(modid, urlToCheck, linePrefix, lineSuffix)).start();
+
+        // notify player about outdated version
         PlayerEvent.PLAYER_JOIN.register(player -> {
             if (CraftedCore.CONFIG != null && CraftedCore.CONFIG.enableVersionChecking) {
-                // get newest version from Uri
+                // get the actual mod version
                 String localVersion = Platform.getMod(modid).getVersion();
-                String newestVersion = localVersion;
-                try {
-                    List<String> versions = checkForNewVersionFromMaven(urlToCheck, linePrefix, lineSuffix, localVersion);
-                    if (!versions.isEmpty()) newestVersion = versions.get(versions.size() - 1);
-                } catch (IOException e) {
-                    // Warns in the log, if checking failed
-                    CraftedCore.LOGGER.error("Failed to get the newest version for " + modName.getString() + " from " + urlToCheck + ".", e);
-                }
+                String newestVersion = cacheNewestVersion(modid, urlToCheck, linePrefix, lineSuffix);
 
-                if (!newestVersion.equals(localVersion)) {
+                if (!localVersion.equals(newestVersion)) {
                     player.sendSystemMessage(Component.translatable(CraftedCore.MODID + ".update", modName, newestVersion));
                 }
             }
         });
     }
 
+    public static String cacheNewestVersion(String modid, URL urlToCheck, String linePrefix, String lineSuffix) {
+        if (CACHED_VERSION.get(modid) == null) {
+            // get the actual mod version
+            String localVersion = Platform.getMod(modid).getVersion();
+            String newestVersion = localVersion;
+            // get newest version from URL
+            try {
+                List<String> versions = checkForNewVersionFromURL(urlToCheck, linePrefix, lineSuffix, localVersion);
+                if (!versions.isEmpty()) newestVersion = versions.get(versions.size() - 1);
+            } catch (IOException e) {
+                // Warns in the log, if checking failed
+                CraftedCore.LOGGER.error("Failed to get the newest version for " + modid + " from " + urlToCheck + ".", e);
+            }
+
+            CACHED_VERSION.put(modid, newestVersion);
+            return newestVersion;
+        } else return CACHED_VERSION.get(modid);
+    }
+
     @Deprecated
     public static String checkForNewVersion(URL urlToCheck, String linePrefix, String lineSuffix) throws IOException {
-        List<String> versions = checkForNewVersionFromMaven(urlToCheck, linePrefix, lineSuffix);
+        List<String> versions = checkForNewVersionFromURL(urlToCheck, linePrefix, lineSuffix);
         return !versions.isEmpty() ? versions.get(versions.size() - 1) : "";
     }
 
-    public static List<String> checkForNewVersionFromMaven(URL mavenURL, String linePrefix, String lineSuffix, String... localVersions) throws IOException {
+    public static List<String> checkForNewVersionFromURL(URL mavenURL, String linePrefix, String lineSuffix, String... localVersions) throws IOException {
         String line;
         List<String> versions = new ArrayList<>();
         BufferedReader updateReader = new BufferedReader(new InputStreamReader(mavenURL.openStream(), StandardCharsets.UTF_8));
