92edd245228070cf93fb14842fd07ac1fc4cc42a
ToCraft <piminecraft492@gmail.com>
2024-05-05 19:45:26 +0200

    create Event Engine

---
+++ common/src/main/java/tocraft/craftedcore/event/Event.java
@@ -0,0 +1,22 @@
+package tocraft.craftedcore.event;
+
+import java.util.List;
+import java.util.function.Function;
+
+@SuppressWarnings({"UnusedReturnValue", "unused"})
+public class Event<T> {
+    private final Function<List<T>, T> invoker;
+    private List<T> handlers;
+
+    protected Event(Function<List<T>, T> invoker) {
+        this.invoker = invoker;
+    }
+
+    public void register(T handler) {
+        handlers.add(handler);
+    }
+
+    public T invoke() {
+        return invoker.apply(handlers);
+    }
+}
