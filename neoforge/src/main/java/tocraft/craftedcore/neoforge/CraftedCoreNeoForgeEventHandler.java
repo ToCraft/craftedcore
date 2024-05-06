e97d9000765ed082ca4c22e3ebdd71a9a2b118cc
ToCraft <piminecraft492@gmail.com>
2024-05-06 16:38:56 +0200

    add LivingDeath event

--- neoforge/src/main/java/tocraft/craftedcore/neoforge/CraftedCoreNeoForgeEventHandler.java
+++ neoforge/src/main/java/tocraft/craftedcore/neoforge/CraftedCoreNeoForgeEventHandler.java
@@ -1,8 +1,11 @@
 package tocraft.craftedcore.neoforge;
 
+import net.minecraft.world.InteractionResult;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import tocraft.craftedcore.data.SynchronizedJsonReloadListener;
+import tocraft.craftedcore.event.common.EntityEvents;
 import tocraft.craftedcore.registration.SynchronizedReloadListenerRegistry;
 
 @SuppressWarnings("unused")
@@ -11,6 +14,14 @@ public class CraftedCoreNeoForgeEventHandler {
     public void addReloadListenerEvent(AddReloadListenerEvent event) {
         for (SynchronizedJsonReloadListener lister : SynchronizedReloadListenerRegistry.getAllListener()) {
             event.addListener(lister);
+        }
+    }
+
+    @SubscribeEvent
+    public void livingDeath(LivingDeathEvent event) {
+        InteractionResult result = EntityEvents.LIVING_DEATH.invoke().die(event.getEntity(), event.getSource());
+        if (result == InteractionResult.FAIL) {
+            event.setCanceled(true);
         }
     }
 }
--- testmod-common/src/main/java/tocraft/craftedcore/testmod/TestMod.java
+++ testmod-common/src/main/java/tocraft/craftedcore/testmod/TestMod.java
@@ -8,6 +8,7 @@ import net.minecraft.world.InteractionResult;
 import org.slf4j.Logger;
 import tocraft.craftedcore.event.client.ClientPlayerEvents;
 import tocraft.craftedcore.event.client.RenderEvents;
+import tocraft.craftedcore.event.common.EntityEvents;
 import tocraft.craftedcore.event.common.PlayerEvents;
 
 import java.util.Objects;
@@ -19,10 +20,15 @@ public class TestMod {
     public static final String MODID = "testmod";
 
     public static void initialize() {
-        PlayerEvents.PLAYER_JOIN.register(player -> LOGGER.info("player " + (player != null ? Objects.requireNonNull(player.getDisplayName()).getString() : "") + " joined."));
-        PlayerEvents.PLAYER_QUIT.register(player -> LOGGER.info("player " + (player != null ? Objects.requireNonNull(player.getDisplayName()).getString() : "") + " quit."));
+        PlayerEvents.PLAYER_JOIN.register(player -> LOGGER.info("player " + (player != null ? player.getName().getString() : "") + " joined."));
+        PlayerEvents.PLAYER_QUIT.register(player -> LOGGER.info("player " + (player != null ? player.getName().getString() : "") + " quit."));
         PlayerEvents.AWARD_ADVANCEMENT.register((player, advancement, criterionKey) -> LOGGER.info((player != null ? player.getDisplayName() : "") + " unlocked advancement " + criterionKey));
         PlayerEvents.REVOKE_ADVANCEMENT.register((player, advancement, criterionKey) -> LOGGER.info((player != null ? player.getDisplayName() : "") + " revoked advancement " + criterionKey));
+
+        EntityEvents.LIVING_DEATH.register((entity, source) -> {
+            LOGGER.info((entity != null ? entity.getName().getString() : "") + "Oh, I just died in your arms tonight.");
+            return InteractionResult.PASS;
+        });
 
         if (Platform.getEnv() == EnvType.CLIENT) {
             TestModClient.initialize();
@@ -32,8 +38,8 @@ public class TestMod {
     @Environment(EnvType.CLIENT)
     static class TestModClient {
         public static void initialize() {
-            ClientPlayerEvents.CLIENT_PLAYER_JOIN.register(player -> LOGGER.info("client player " + (player != null ? Objects.requireNonNull(player.getDisplayName()).getString() : "") + " joined."));
-            ClientPlayerEvents.CLIENT_PLAYER_QUIT.register(player -> LOGGER.info("client player " + (player != null ? Objects.requireNonNull(player.getDisplayName()).getString() : "") + " quit."));
+            ClientPlayerEvents.CLIENT_PLAYER_JOIN.register(player -> LOGGER.info("client player " + (player != null ? player.getName().getString() : "") + " joined."));
+            ClientPlayerEvents.CLIENT_PLAYER_QUIT.register(player -> LOGGER.info("client player " + (player != null ? player.getName().getString() : "") + " quit."));
             ClientPlayerEvents.CLIENT_PLAYER_RESPAWN.register((oldPlayer, newPlayer) -> LOGGER.info((newPlayer != null ? Objects.requireNonNull(newPlayer.getDisplayName()).getString() : "") + " respawned and was previously named " + (oldPlayer != null ? Objects.requireNonNull(oldPlayer.getDisplayName()).getString() : "")));
 
             RenderEvents.RENDER_MOUNT_HEALTH.register((player, graphics) -> InteractionResult.FAIL);
