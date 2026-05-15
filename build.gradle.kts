99a64af7f22bbbfff29d45e12b39488e2b22e2da
To_Craft <development@tocraft.dev>
2026-05-15 21:02:15 +0200

    update ModMaster

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
 plugins {
-    id("dev.tocraft.modmaster.root") version ("2.3")
+    id("dev.tocraft.modmaster.root") version ("2.4-SNAPSHOT")
 }
 
+// build.gradle.kts
+tasks.register("showExactPluginVersion") {
+    doLast {
+        println("RUNS")
+        buildscript.configurations.findByName("classpath")?.resolvedConfiguration?.resolvedArtifacts?.forEach { artifact ->
+            if (artifact.moduleVersion.id.name.contains("modmaster")) {
+                println("--- Plugin Info ---")
+                println("Declared Version: ${artifact.moduleVersion.id.version}")
+                println("Exact File Name:  ${artifact.file.name}")
+                println("-------------------")
+            }
+        }
+    }
+}
 subprojects {
     repositories {
         maven("https://maven.fabricmc.net/") // fabric api
